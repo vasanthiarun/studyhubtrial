f7cf6bb458e847ce7a55141035e4d96895ff9109
maria <maria@gsmhub.me>
2025-09-27 15:57:04 +0530

    changed as const App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Header from './components/header/header.component.jsx';
 import RegisterForm from './components/auth/register.component.jsx';
 
 
-function App() {
-  return (
+
+const App = () => {
+   return (
     <div>
      <Header />  
 	 <RegisterForm /> 
     </div>
-  );
-
-}
+   );
+};
 
 export default App;
+
+
