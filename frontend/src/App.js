eac692e07513aca6bc29ae830130a6da276dff5e
maria <maria@gsmhub.me>
2025-09-27 15:32:09 +0530

    Header component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Header from './components/header/header.component.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header />     
     </div>
   );
+
 }
 
 export default App;
---
+++ frontend/src/components/header/header.component.jsx
@@ -0,0 +1,32 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+const Header = () => {
+  return (
+    <Navbar expand="lg" bg="light" className="border-bottom shadow-sm">
+      <Container>
+        {/* Brand on left */}
+        <Navbar.Brand href="/">StudyHub</Navbar.Brand>
+
+        {/* Hamburger toggler */}
+        <Navbar.Toggle aria-controls="main-navbar-nav" />
+
+        {/* Collapsible content */}
+        <Navbar.Collapse id="main-navbar-nav">
+          {/* Center menu */}
+          <Nav className="mx-auto">
+            <Nav.Link href="/question-bank">Question Bank</Nav.Link>
+            <Nav.Link href="/academic-resources">Academic Resources</Nav.Link>
+            <Nav.Link href="/discuss">Discuss</Nav.Link>
+          </Nav>
+
+          {/* Right side login/register */}
+          <Nav className="ms-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
